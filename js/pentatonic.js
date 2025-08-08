cdbcfab57fe6a1dedb8b292cf83204f05f90fc6b
Sebas Zavala <63665504+ZavalaSebas@users.noreply.github.com>
2025-08-08 17:37:17 -0600

    Update pentatonic.js

--- js/pentatonic.js
+++ js/pentatonic.js
@@ -262,10 +262,16 @@ let artefactosOriginal = [
   video: "https://www.youtube.com/embed/SbYa7NBYyRc"
 },
 {
-titulo: "Time Adventure"
-banda: "Rebecca Sugar"
-descripcion: "Rebecca Sugar performs 'Time Adventure' from Adventure Time, presentado por Cartoon Network."
+titulo: "Time Adventure",
+banda: "Rebecca Sugar",
+descripcion: "Rebecca Sugar performs 'Time Adventure' from Adventure Time, presentado por Cartoon Network.",
 video: "https://www.youtube.com/embed/Xr53S9vIbCE"
+},
+{
+titulo: "Hasta la Raíz (Austin City Limits)",
+banda: "Natalia Lafourcade",
+descripcion: "Presentación en Austin City Limits donde Natalia interpreta 'Hasta la Raíz', tema titular de su álbum ganador de un Grammy.",
+video: "https://www.youtube.com/embed/zayX1YXiP6Y"
 }
 
 ];
